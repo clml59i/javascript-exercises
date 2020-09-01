0e05df2fd295b7345a464e3514f8fbc20c652731
Clement Lance <clml59doc@gmail.com>
2020-09-01 21:36:49 +0700

    Add solution to findTheOldest problem

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    return people.reduce(getOldestPerson);
+}
 
+// return the age of the person
+let getAge = function(person){
+    let currentYear = new Date().getFullYear();
+    if (person.hasOwnProperty('yearOfDeath')){
+        return person.yearOfDeath - person.yearOfBirth;
+    }else {
+        return currentYear - person.yearOfBirth;
+    }
+}
+
+// compare two people and return the oldest one
+let getOldestPerson = function(personA, personB){
+    if(getAge(personA) > getAge(personB)){
+        return personA;
+    }else {
+        return personB;
+    }
 }
 
 module.exports = findTheOldest
